ff629e2156a25b5241b51fd2b7236813ce372c66
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-07-10 13:27:58 -0400

    Odd challenge

---
+++ odd.js
@@ -0,0 +1,10 @@
+const isOdd = function(num){
+  if(num % 2 ==! 0){
+    return "true"
+  } else {
+    return "false"
+  }
+
+}
+console.log("13 is odd: " + isOdd(13));
+console.log("8 is odd: " + isOdd(8));
