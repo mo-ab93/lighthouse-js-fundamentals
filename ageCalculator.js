753d8907e0fcbb99d05a0322782a003f317fdb0b
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-07-10 15:40:17 -0400

    add age calculator function challenge

---
+++ ageCalculator.js
@@ -0,0 +1,13 @@
+// const ageCalculator = function(name, yearOfBirth, currentYear){
+//   return `${name} is ${currentYear - yearOfBirth} years old.`;
+// }
+
+
+const ageCalculator = function (name, yearOfBirth, currentYear){
+  let age = currentYear - yearOfBirth;
+  return name + ' is ' + age + ' years old. '
+}
+
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
