faa8dace762cdf5d3bf2566533eff4607b49319b
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-07-10 12:29:39 -0400

    add some examples for function

---
+++ hello.js
@@ -0,0 +1,13 @@
+// const sayHello = function (){
+//   console.log('Hello, World');
+// }
+
+// sayHello();
+
+const sayHello = function(name){
+  console.log("Hello, " + name);
+}
+
+sayHello("Mohammed");
+sayHello("Ahmed");
+sayHello("Aras");
