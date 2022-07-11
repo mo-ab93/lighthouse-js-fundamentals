f615049032b5e8d9c9e3776b314c64c968f201d0
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-07-11 13:38:26 -0400

    add packing list exercise for loop

---
+++ packing-list.js
@@ -0,0 +1,10 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for(let i = 0; i < packingList.length; i++){
+  console.log(packingList[i]);
+}
+
+// for(let items of packingList){
+//   console.log(items);
+// }
