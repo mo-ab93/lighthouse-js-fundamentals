a9a1060176ebf33fddfa44de674de7dbe50f92e1
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-07-20 20:40:12 -0400

    add check air challenge

---
+++ checkAir.js
@@ -0,0 +1,29 @@
+const checkAir = function (samples, threshold) {
+  // Code here!
+  let numOfDirty = 0;
+  for(let i  = 0; i < samples.length; i++){
+    if(samples[i] === 'dirty'){
+      numOfDirty++;
+    }
+  }
+  if(numOfDirty/samples.length > threshold){
+    return 'Polluted'
+  }else if (numOfDirty/samples.length <= threshold){
+    return 'Clean'
+  }
+};
+
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+))
