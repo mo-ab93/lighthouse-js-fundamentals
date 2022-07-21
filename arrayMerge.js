9ce32764a38fb302688744c8f2970a6b1f8632b1
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-07-21 14:17:16 -0400

    add merge arrays challenge

---
+++ arrayMerge.js
@@ -0,0 +1,8 @@
+const merge = function (arr1, arr2){
+  let newArr = arr1.concat(arr2);
+  return newArr.sort();
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
