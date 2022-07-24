fcfc3d8c515a3577df3812bea95759cdfb077784
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-07-23 22:52:27 -0400

    add multiplication table challenge

---
+++ multiplicationTable.js
@@ -0,0 +1,15 @@
+
+const multiplicationTable = function(maxValue) {
+  let result = '';
+  for(let i = 1; i <= maxValue; i++){
+    for(let j = 1; j <= maxValue; j++){
+     result += (i * j) + ' ';
+    }
+     result += '\n';
+  }
+  return result;
+}
+
+console.log(multiplicationTable(1));
+console.log(multiplicationTable(5));
+console.log(multiplicationTable(10));
